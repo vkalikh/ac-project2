dbd688274d894e56ca0e79494727f37bbec1646f
Your Name <you@example.com>
2019-11-01 18:02:51 +0300

    Homework 2 is ready

---
+++ src/Homework2/Task1_2.java
@@ -0,0 +1,23 @@
+package Homework2;
+
+import java.util.Scanner;
+import static java.lang.Math.sqrt;
+
+public class Task1_2 {
+    public static void main(String[] args) {
+        int a = 0, b = 0, r = 0;
+        double c = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the first side of rectangle:");
+        a = in.nextInt();
+        System.out.println("Enter the second side of rectangle:");
+        b = in.nextInt();
+        System.out.println("Enter a circle radius:");
+        r = in.nextInt();
+        c = sqrt(a^2 + b^2);
+        if (c <= r*2)
+            System.out.println("Circle could overlap rectangle fully");
+        else
+            System.out.println("Circle couldn't overlap rectangle fully");
+    }
+}
---
+++ src/Homework2/Task3_4.java
@@ -0,0 +1,39 @@
+package Homework2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task3_4 {
+
+    public static void main(String[] args) {
+        int n = 0, i;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number of items:");
+        n = in.nextInt();
+
+        while (n <= 0) {
+            System.out.println("Invalid number. \n Enter one more time:");
+            n = in.nextInt();
+        }
+
+        int[] mas = new int[n];
+        Random rand = new Random();
+
+        for (i = 0; i < n; i++) {
+            mas[i] = rand.nextInt(30);
+            System.out.print(mas[i] + ", ");
+        }
+        System.out.println();
+
+        int min = mas[0], max = mas[0], sum = mas[0];
+        for (i = 1; i < n; i++) {
+            sum = sum + mas[i];
+            if (min > mas[i])
+                min = mas[i];
+            else if (max < mas[i])
+                max = mas[i];
+        }
+        System.out.println("Maximun = " + max + "\nMinimum = " + min + "\nAvarage = " + (float)sum / n);
+    }
+}
